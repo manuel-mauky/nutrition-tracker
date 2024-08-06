b8dec69a4b8d8f6eec64b1b52024482d38da5cdf
manuel <github@lestard.eu>
2024-08-06 23:41:24 +0200

    project init

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import { CustomProvider } from "rsuite"
+
+import "rsuite/dist/rsuite.min.css"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <CustomProvider theme="light">
+      <App />
+    </CustomProvider>
+  </React.StrictMode>,
+)
