9e14b25bd0092546f64dc5796c89a66417901f11
manuel <github@lestard.eu>
2025-06-16 17:01:49 +0200

    switch to hash-router to support github pages, update tanstack router

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import { CustomProvider } from "rsuite"
-import { createRouter, RouterProvider } from "@tanstack/react-router"
+import { createHashHistory, createRouter, RouterProvider } from "@tanstack/react-router"
 import deDE from "rsuite/esm/locales/de_DE"
 import { routeTree } from "./routeTree.gen.ts"
 import { useStore } from "./features/store.ts"
 
-const router = createRouter({ routeTree })
+const hashHistory = createHashHistory()
+
+const router = createRouter({ routeTree, history: hashHistory })
 
 declare module "@tanstack/react-router" {
   interface Register {
